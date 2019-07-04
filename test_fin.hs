d9484c6c5d9fca7df1870e90779e739f11d514fb
Erik Vesterlund <evesterl@cisco.com>
2019-07-05 00:49:53 +0200

    good to go

--- test_fin.hs
+++ test_fin.hs
@@ -1,7 +1,10 @@
-import FinLib
+import System.Environment (getArgs)
+
+import qualified FinLib as Fin
 
 -- use this style rather than records to minimize effort
 -- involved in writing data files by hand
+-- TODO: obviously gonna need name :: String
 data Purchase = Purchase String Double Double deriving (Show, Read)
 
 date :: Purchase -> String
@@ -13,8 +16,15 @@ price (Purchase _ p _) = p
 pieces :: Purchase -> Double
 pieces (Purchase _ _ x) = x
 
+-- temporary dummy method since FinLib doesn't use `Purchase`
+toNum :: Purchase -> (Double, Double)
+toNum p = (price p, pieces p)
+
 main = do
-    let dummyfile = "../tmp"
+    args <- getArgs
+    let dummyfile = if length args > 0
+               then head args
+               else "../tmp"
     -- how to create the dummy data if need be:
     --writeFile dummyfile "Purchase \"foo\" 1.1 3.14\n\nPurchase \"foo\" 1.2 3.15\n"
 
@@ -22,6 +32,6 @@ main = do
     let inputlines = lines input
         fmtd :: [Purchase] -- necessary specialization for `read`
         fmtd = map read (filter (\x -> length x > 0) inputlines)
-    putStrLn $ "formatted: " ++ show fmtd
-
-    -- TODO: sort by date, then group by date
+    let a = map toNum fmtd
+    -- pretty much all set up to do math, finally
+    return a
